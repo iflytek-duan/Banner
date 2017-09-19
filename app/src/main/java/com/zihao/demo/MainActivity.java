2b23bb48db269a6c50ae8ffab47fc508a1063abe
zihao <jgduan@iflytek.com>
2017-09-19 11:14:23 +0800

    新增banner.setEnableGallery方法调用，体验新增画廊效果。

--- app/src/main/java/com/zihao/demo/MainActivity.java
+++ app/src/main/java/com/zihao/demo/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "onPageClick:" + position);
             }
         });
+        banner.setEnableGallery(true);
 
     }
 
