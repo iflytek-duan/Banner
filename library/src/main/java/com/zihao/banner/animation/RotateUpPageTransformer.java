500cd167c217ab4fc03febccf12b28e5c0624452
zihao <jgduan@iflytek.com>
2017-09-20 15:15:35 +0800

    新增向上翻转动画。

---
+++ library/src/main/java/com/zihao/banner/animation/RotateUpPageTransformer.java
@@ -0,0 +1,41 @@
+package com.zihao.banner.animation;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/**
+ * ClassName：RotateUpPageTransformer
+ * Description：TODO<向上旋转切换动画>
+ * Author：zihao
+ * Date：2017/9/20 15:09
+ * Version：v1.0
+ */
+public class RotateUpPageTransformer implements ViewPager.PageTransformer {
+
+    private static final float DEFAULT_MAX_ROTATE = 15.0f;
+
+    @Override
+    public void transformPage(View page, float position) {
+        int pageWidth = page.getWidth();
+        int pageHeight = page.getHeight();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            page.setRotation(DEFAULT_MAX_ROTATE);
+            page.setPivotX(pageWidth);
+        } else if (position <= 1) { // [-1,1]
+            if (position < 0) {//[0，-1]
+                page.setPivotX(pageWidth * (0.5f + 0.5f * (-position)));
+                page.setRotation(-DEFAULT_MAX_ROTATE * position);
+            } else {//[1,0]
+                page.setPivotX(pageWidth * 0.5f * (1 - position));
+                page.setRotation(-DEFAULT_MAX_ROTATE * position);
+            }
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            page.setRotation(-DEFAULT_MAX_ROTATE);
+            page.setPivotX(0);
+        }
+        page.setPivotY(0);
+    }
+}
